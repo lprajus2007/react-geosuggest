f80d5530ba3d267d53b083675eb49f8832863c3f
Satya <lprajus2007@gmail.com>
2016-02-25 13:50:06 -0500

    Add all files

---
+++ example/src/app.js
@@ -0,0 +1,59 @@
+/* global google */
+
+var React = require('react'),
+  Geosuggest = require('../../src/Geosuggest'); // eslint-disable-line
+
+var App = React.createClass({ // eslint-disable-line
+  /**
+   * Render the example app
+   * @return {Function} React render function
+   */
+  render: function() {
+    var fixtures = [
+      {label: 'New York', location: {lat: 40.7033127, lng: -73.979681}},
+      {label: 'Rio', location: {lat: -22.066452, lng: -42.9232368}},
+      {label: 'Tokyo', location: {lat: 35.673343, lng: 139.710388}}
+    ];
+
+    return ( // eslint-disable-line
+      <div>
+        <Geosuggest
+          fixtures={fixtures}
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+          onChange={this.onChange}
+          onSuggestSelect={this.onSuggestSelect}
+          location={new google.maps.LatLng(53.558572, 9.9278215)}
+          radius="20" />
+      </div>
+    );
+  },
+
+  /**
+   * When the input receives focus
+   */
+  onFocus: function() {
+    console.log('onFocus'); // eslint-disable-line
+  },
+
+  /**
+   * When the input loses focus
+   */
+  onBlur: function() {
+    console.log('onBlur'); // eslint-disable-line
+  },
+
+  onChange: function(value) {
+    console.log('input changes to :' + value); // eslint-disable-line
+  },
+
+  /**
+   * When a suggest got selected
+   * @param  {Object} suggest The suggest
+   */
+  onSuggestSelect: function(suggest) {
+    console.log(suggest); // eslint-disable-line
+  }
+});
+
+React.render(<App />, document.getElementById('app')); // eslint-disable-line
